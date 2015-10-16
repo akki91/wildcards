ffc7ea8897fe619f09236cd4ce15676f4554ebf9
akki91 <akshay.khare91@gmail.com>
2015-10-17 00:21:41 +0530

    rakc cors and user suggestion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,15 @@ class UsersController < ApplicationController
   end
 
   def autocomplete
-    render json: {}, status: 200
+   # keyword = params[:git_name]
+    keyword = "#{params["git_name"].upcase}%" rescue nil 
+    if keyword
+      users_auto_search = User.where("UPPER(users.git_username) LIKE ?",keyword).limit(5).select(["users.id as id, users.email as email,users.git_username as username,users.avatar_url as avatar_url"  ])
+      render json: {"result" => users_auto_search}, status: :ok
+    else
+      render json: {"message" => "No Querying Parameter provided"}, status: :unprocessable_entity
+    end
+
   end
 
 end
