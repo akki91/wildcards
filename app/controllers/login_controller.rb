9d9896ac2f0a4bab4c585389459108f207132037
akki91 <akshay.khare91@gmail.com>
2015-10-16 19:08:45 +0530

    added redis

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,18 +3,15 @@ class LoginController < ApplicationController
   def git_callback
     session_code = request.env['rack.request.query_hash']['code']
     site = RestClient::Resource.new('https://github.com/login/oauth/access_token')
-    	result = 		     site.post(
-                             {:client_id => "cffe0ca2e4b88babd27c",
-                              :client_secret => "1599750bea795965735cbf9391c72f648f6c1327",
-                              :code => session_code},
-                              :accept => :json)
-
-    # extract the token and granted scopes
+    result =         site.post(
+      {:client_id => "cffe0ca2e4b88babd27c",
+       :client_secret => "1599750bea795965735cbf9391c72f648f6c1327",
+       :code => session_code},
+       :accept => :json)
     access_token = JSON.parse(result)['access_token']
-    render json: {:message => "Authentication Success" ,:token => access_token }, :status => 200 
+    redirect_to "http://localhost:4000?#{access_token}"
   end
-
-  
 
 
 end
+
